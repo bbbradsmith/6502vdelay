ce07adff702433b5769e72c3fd0e00a19bac0478
bbbradsmith <bbbradsmith@users.noreply.github.com>
2024-03-29 14:27:16 -0400

    make test batch also run the tests

--- test/compile.bat
+++ test/compile.bat
@@ -1,21 +1,20 @@
-@del temp\vdelay.o
-@del temp\test.c.s
-@del temp\test.c.o
-@del temp\test.o
-@del temp\test.bin
-@del temp\test.map
-@del temp\test_nes.o
-@del temp\test_nes.map
-@del temp\test_nes.dbg
-@del temp\test_nes.nes
+REM full test
+SET /A count = 65536
+
+@REM abbreviated test
+@REM SET /A count = 1000
+
+REM Clean temp directory
+
+@del temp\* /Q
 
 
-REM if you wish to test alternative versions, uncomment them to replace vdelay.s
+REM Test code
 
 cc65\bin\ca65 -o temp\vdelay.o -g ..\vdelay.s
 @IF ERRORLEVEL 1 GOTO error
 
-REM cc65\bin\ca65 -o temp\vdelay.o -g ..\vdelay_short.s
+cc65\bin\ca65 -o temp\vdelay_short.o -g ..\vdelay_short.s
 @IF ERRORLEVEL 1 GOTO error
 
 cc65\bin\ca65 -o temp\test.o -g test.s
@@ -30,27 +29,71 @@ cc65\bin\cc65 -o temp\test.c.s -T -O -g test.c
 cc65\bin\ca65 -o temp\test.c.o -g temp\test.c.s
 @IF ERRORLEVEL 1 GOTO error
 
-
 REM 6502 simulation
+
 cc65\bin\ld65 -o temp\test.bin -C test.cfg -m temp\test.map temp\vdelay.o temp\test.o temp\test.c.o sim6502.lib
 @IF ERRORLEVEL 1 GOTO error
 
-REM 65C02 simulation
-REM cc65\bin\ld65 -o temp\test.bin -C test.cfg -m temp\test.map temp\vdelay.o temp\test.o temp\test.c.o sim65C02.lib
+cc65\bin\ld65 -o temp\test_short.bin -C test.cfg -m temp\test_short.map temp\vdelay_short.o temp\test.o temp\test.c.o sim6502.lib
 @IF ERRORLEVEL 1 GOTO error
+
+REM 65C02 simulation
+
+cc65\bin\ld65 -o temp\testc.bin -C test.cfg -m temp\testc.map temp\vdelay.o temp\test.o temp\test.c.o sim65C02.lib
+@IF ERRORLEVEL 1 GOTO error
+
+cc65\bin\ld65 -o temp\testc_short.bin -C test.cfg -m temp\testc_short.map temp\vdelay_short.o temp\test.o temp\test.c.o sim65C02.lib
+@IF ERRORLEVEL 1 GOTO error
+
+REM NES demo
 
 cc65\bin\ld65 -o temp\test_nes.nes -C test_nes.cfg --dbgfile temp\test_nes.dbg -m temp\test_nes.map temp\vdelay.o temp\test_nes.o
 @IF ERRORLEVEL 1 GOTO error
 
+@echo.
+@echo.
+@echo Build successful! Creating test batches...
+@echo.
+@echo.
+
+test.py bat test_short 256
+test.py bat test %count%
+test.py bat testc_short 256
+test.py bat testc %count%
 
 @echo.
 @echo.
-@echo Build successful!
+@echo Running tests...
+@echo.
+@echo.
+
+test.py run test_short
+test.py run test
+test.py run testc_short
+test.py run testc
+
+@echo.
+@echo.
+@echo Test logs...
+@echo.
+@echo.
+
+test.py log test_short
+test.py log test
+test.py log testc_short
+test.py log testc
+
+@echo.
+@echo.
+@echo Tests complete. Check results above.
+@echo.
 @pause
 @GOTO end
+
 :error
 @echo.
 @echo.
 @echo Build error!
+@echo.
 @pause
 :end
--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 # 6502 vdelay simulation test
-# Brad Smith, 2020
+# Brad Smith, 2020, 2024
 # https://github.com/bbbradsmith/6502vdelay
 #
 # This will test every input value of vdelay.
@@ -21,53 +21,76 @@
 # You can press CTRL+C while the batch file is running to stop the test run,
 # which will analyze the partially generated log.
 
+import sys
 import os
 import subprocess
 
-skip_run = False # True to test a partial log already generated
+TEMP = "temp\\"
+SIM65 = "cc65\\bin\\sim65 -c "
 
-bat = "temp\\test.bat"
-log = "temp\\test.log"
-run = "cc65\\bin\\sim65 -c temp\\test.bin"
+def create_bat(testname,count):
+    bat = TEMP + testname + ".bat"
+    log = TEMP + testname + ".log"
+    run = SIM65 + TEMP + testname + ".bin"
+    try:
+        os.remove(bat)
+    except:
+        pass
+    s = "%s 0000N >> %s\n" % (run,log) # null test
+    for i in range(0,count):
+        cline = "%s %04X >> %s\n" % (run,i,log)
+        s += cline
+    f = open(bat,"wt")
+    f.write(s)
+    f.close()
+    print("Test batch created: " + bat)
 
-try:
-    os.remove(bat)
-except:
-    pass
-
-if not skip_run:
+def run_bat(testname):
+    bat = TEMP + testname + ".bat"
+    log = TEMP + testname + ".log"
     try:
         os.remove(log)
     except:
         pass
-
-s = "%s 0000N >> %s\n" % (run,log) # null test
-for i in range(0,65536):
-    cline = "%s %04X >> %s\n" % (run,i,log)
-    s += cline
-f = open(bat,"wt")
-f.write(s)
-f.close()
-
-if not skip_run:
+    print("Test batch run: " + bat + " > " + log)
     subprocess.call(bat)
 
-logged = open(log,"rt").readlines()
+def read_log(testname):
+    log = TEMP + testname + ".log"
+    print("Test batch result: " + log)
+    logged = open(log,"rt").readlines()
+    ls = logged[0].split()
+    last = int(ls[0])
+    last_diff = 0
+    print("----- NULL: %d" % last)
+    count = 0
+    for i in range(1,len(logged)):
+        ls = logged[i].split()
+        if len(ls) == 2:
+            count += 1
+            cycles = int(ls[0])
+            diff = cycles - last
+            if diff != last_diff:
+                print("%5d %04X: %d" % (i-1,i-1,diff))
+            last = cycles
+            last_diff = diff
+    print("count: %d" % count)
 
-ls = logged[0].split()
-last = int(ls[0])
-last_diff = 0
-print("----- NULL: %d" % last)
+def printargs():
+    print("Usage: test.py [mode] [test name] [count]")
+    print("  test.py bat test 65536 -> generates test batch files for test.bin")
+    print("  test.py run test -> use bat mode first, runs test and prints log")
+    print("  test.py log test -> reprints log from past test run")
 
-count = 0
-for i in range(1,len(logged)):
-    ls = logged[i].split()
-    if len(ls) == 2:
-        count += 1
-        cycles = int(ls[0])
-        diff = cycles - last
-        if diff != last_diff:
-            print("%5d %04X: %d" % (i-1,i-1,diff))
-        last = cycles
-        last_diff = diff
-print("count: %d" % count)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        printargs()
+    elif sys.argv[1] == "bat":
+        create_bat(sys.argv[2],int(sys.argv[3]))
+    elif sys.argv[1] == "run":
+        run_bat(sys.argv[2])
+        read_log(sys.argv[2])
+    elif sys.argv[1] == "log":
+        read_log(sys.argv[2])
+    else:
+        printargs()
